6b99ddf57712ce5adcaf42c85a0057b07c91b99a
Lakshaman Kere <111192180+Laxmankere@users.noreply.github.com>
2025-03-31 23:56:26 +0530

    updated comment

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -3,7 +3,7 @@ package com.qa.opencart.pages;
 public class DemoPage {
 
 	public static void main(String[] args) {
-		System.out.println("I am king");
+		System.out.println("I am king Lucky");
 
 	}
 
